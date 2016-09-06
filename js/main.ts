46617c01c4cbd844b37a87edd83ddd77436e7248
Mike Lee <elee353@aucklanduni.ac.nz>
2016-09-07 08:49:58 +1200

    commit weatherApp

---
+++ js/main.ts
@@ -0,0 +1,38 @@
+///<reference path="/jquery.d.ts" />
+var $ = jQuery.noConflict();
+
+if ("geolocation" in navigator) {
+    navigator.geolocation.getCurrentPosition(function (position) {
+        loadWeather(position.coords.latitude, position.coords.longitude);
+    });
+} else{
+    loadWeather("Bangalore,IN","");
+}
+
+$(document).ready(function(){
+    setInterval(getWeather ,10000);
+});
+
+function loadWeather(location : any, woeid : any){
+    $.simpleWeather({
+        location: location,
+        woeid: woeid,
+        unit: 'C',
+        success: function(weather){
+            var city = weather.city;
+            var temp = weather.temp + '&deg;';
+            var wcode = '<img class="weathericon" src="images/weathericons/'+weather.code+'.svg">';
+            var wind = '<p>'+weather.wind.speed+'</p><p>'+weather.units.speed+'</p>';
+            var humidity = weather.humidity+' %';
+            $(".location").text(city);
+            $(".temperature").html(temp);
+            $(".climate_bg").html(wcode);
+            $(".windspeed").html(wind);
+            $(".humidity").text(humidity);
+        },
+        error: function(error){
+            $(".error").html('<p>'+error+'</p>');
+        }
+        
+    });
+}
